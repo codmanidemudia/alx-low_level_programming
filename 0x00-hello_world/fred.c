e844f549762b2b6e0d799410eace1f356df3dfe1
Fredrick Idemudia <alertmail1000@gmail.com>
2022-09-08 10:32:14 -0700

    task 0 done

---
+++ 0x00-hello_world/fred.c
@@ -0,0 +1,16 @@
+/**
+ *main - entry point
+ *
+ * reture: always 0 success
+ */
+#include <stdio.h>
+
+int main(void)
+{
+int a = 10;
+int b = 20;
+int sum = a + b;
+printf("The sum is %d\n", sum)
+return (0)
+}
+
