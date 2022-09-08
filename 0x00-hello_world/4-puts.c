341c34c3fad4d2c43b85a8df9ff46eb24c9f060a
Fredrick Idemudia <alertmail1000@gmail.com>
2022-09-08 12:43:57 -0700

    task 4 done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry Point
  *
- * Return : 0 after printing the function
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-puts("programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
