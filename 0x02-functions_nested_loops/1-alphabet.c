e71bb5c3bd68ba009b0cdd5feb0cf2915f2c508a
Fredrick Idemudia <alertmail1000@gmail.com>
2022-09-13 19:44:54 -0700

    task 1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - print lower  aphabet
+ * Return: Always 0
+ *
+ */
+
+int print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
+
