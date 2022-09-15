ef2b1684283dc9277e577f5adf901cf4ab59bb50
Fredrick Idemudia <alertmail1000@gmail.com>
2022-09-15 14:05:56 -0700

    task 1 done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 /**
-* main - while loop
+* main - tests function that prints if integer is positive or negative
 * Return: 0
-* i: i for initial
 */
 int main(void)
 {
 int i;
+
 i = 0;
-/**
-*while (i < 10)
-*{
-*putchar(i);
-*}
-*/
+positive_or_negative(i);
 return (0);
 }
