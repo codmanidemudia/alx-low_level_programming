dd254755fd8d9ca1cbdea1c3b006e91be5e8bb02
Fredrick Idemudia <alertmail1000@gmail.com>
2022-09-14 17:52:09 -0700

    task 8 done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,7 +20,7 @@ hours_remainder = hours % 10;
 _putchar(hours / 10 + '0');
 _putchar(hours_remainder + '0');
 _putchar(':');
-_putchar(minutes / 10 '0');
+_putchar(minutes / 10 + '0');
 _putchar(mins_remainder + '0');
 menutes++;
 _putchar('\n');
