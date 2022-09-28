7f4613d60ae6e518dfdfbfbf01cb476ec85c375b
Fredrick Idemudia <alertmail1000@gmail.com>
2022-09-28 11:34:13 -0700

    task 0 done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*_put_recursion - function to print text
+*@s: parameter used for function
+* Return: Always 0.
+*/
+
+void _put_recursion(char *s)
+{
+char result = puts(s);
+return (result);
+}
